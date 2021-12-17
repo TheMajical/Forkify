0661463991234e5085dd9ded1767b8cef2f01fd6
TheMajical <pinkpanther7729@gmail.com>
2021-12-17 18:04:05 +0330

    Bundling with Parcel and setting up first API call

--- src/js/controller.js
+++ src/js/controller.js
@@ -11,3 +11,33 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+async function showRecipe(){
+  try{
+    const res = await fetch('https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886');
+    const data = await res.json();
+  
+    if(!res.ok) throw new Error(`Wrong URL Id - ${res.status}`);
+    console.log(res, data);
+
+    let {recipe} = data.data;
+
+    recipe = {
+      id: recipe.id,
+      title: recipe.title,
+      publisher: recipe.publisher,
+      sourceUrl: recipe.source_url,
+      image: recipe.image_url,
+      servings: recipe.servings,
+      cookingTime: recipe.cooking_time,
+      ingredients: recipe.ingredients
+    }
+
+    console.log(recipe);
+  }
+  catch(err){
+    alert(`${err.message}`);
+  }
+}
+
+showRecipe();
